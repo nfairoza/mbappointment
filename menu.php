7c84c648c823447849a2a95c82e0e39fe451842d
Fairoza <nfairoza@f01898084dfa.ant.amazon.com>
2019-05-15 19:09:24 -0400

    added ip address

--- menu.php
+++ menu.php
@@ -1,7 +1,15 @@
 <nav class="navbar navbar-default" role="navigation">
 
 <div class="navbar-header">
-<a class="navbar-brand" href="/"><i class="fas fa-box-open"></i> DataBase</a>
+<a class="navbar-brand" href="http://54.236.250.211"><i class="fas fa-cut"></i> Cutting Edge</a>
+</div>
+
+<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+  <ul class="nav navbar-nav">
+    <li>
+      <a href="/"><i class="fas fa-braille"></i> Appointment Schedule</a>
+    </li>
+  </ul>
 </div>
 
 <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
